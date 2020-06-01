240703a4db02095489b0780b92508063632ced0a
William Pereira <williamcpereira89@gmail.com>
2020-06-01 15:43:07 -0300

    initial commit

---
+++ craco.config.js
@@ -0,0 +1,20 @@
+/* eslint-disable */
+const path = require('path')
+
+const joinResolver = _path => path.join(path.resolve(__dirname, _path))
+
+module.exports = {
+    webpack: {
+        alias: {
+            '@app': joinResolver('./src'),
+            '@components': joinResolver('./src/components'),
+            '@containers': joinResolver('./src/containers'),
+            '@helpers': joinResolver('./src/helpers'),
+            '@images': joinResolver('./src/images'),
+            '@pages': joinResolver('./src/pages'),
+            '@routes': joinResolver('./src/routes'),
+            '@services': joinResolver('./src/services'),
+            '@styles': joinResolver('./src/styles')
+        }
+    }
+}
